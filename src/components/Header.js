c569e5d91fcbc18e0fc7733d0e91cd7bc51c88be
Daniel Doblado <danieldoblado@gmail.com>
2017-09-20 10:39:49 +0200

    Modify links in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,56 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import withEmitter from './withEmitter'
 import translate from './translate'
+import Link from './Link'
 
 import '../styles/Header.pcss'
 
 const Header = ({ user, emitter, translate }) => (
   <header className="Header">
     <nav className="mainNav">
-      <a href="/" title={translate('Header.home')}>
+      <Link to="/">
         <h1>OER W<i className="fa fa-globe" />RLD MAP</h1>
+      </Link>
+      <a
+        href="/FAQ"
+        title={translate('Header.faq')}
+      >
+        {translate('Header.faq')}
       </a>
-      <a href="#" title={translate('Header.faq')}>{translate('Header.faq')}</a>
-      <a href="#" title={translate('Header.about')}>{translate('Header.about')}</a>
-      <a href="#" title={translate('Header.contribute')}>{translate('Header.contribute')}</a>
-      <a href="#" title={translate('Header.blog')}>{translate('Header.blog')}</a>
-      <a href="#" title="Facebook">
+      <a
+        href="/about"
+        title={translate('Header.about')}
+      >
+        {translate('Header.about')}
+      </a>
+      <a
+        href="/contribute"
+        title={translate('Header.contribute')}
+      >
+        {translate('Header.contribute')}
+      </a>
+      <a
+        href="https://oerworldmap.wordpress.com/"
+        title={translate('Header.blog')}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {translate('Header.blog')}
+      </a>
+      <a
+        href="https://www.facebook.com/oerworldmap"
+        title="Facebook"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fa fa-facebook-official" />
       </a>
-      <a href="#" title="Twitter">
+      <a
+        href="https://twitter.com/oerworldmap"
+        title="Twitter"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fa fa-twitter" />
       </a>
     </nav>
