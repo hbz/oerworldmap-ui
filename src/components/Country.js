f3f91b9670e418eb2f1b0f67594f963a757b4b65
Daniel Doblado <danieldoblado@gmail.com>
2017-10-24 16:34:17 +0200

    Move Country.js to React Component

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,5 +1,3 @@
-/* global document */
-
 import React from 'react'
 import PropTypes from 'prop-types'
 
@@ -15,138 +13,135 @@ const triggerClick = (e) => {
   }
 }
 
-const hideContainer = (header, container) => {
+class Country  extends React.Component {
 
-  const icon = header.target.querySelector('i')
-    || (header.target.classList.contains('fa')
-      ? header.target
-      : header.target.parentElement.querySelector('i'))
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCountryChampion: true,
+      showReports: false,
+      showStatistics: false,
+    }
+  }
 
-  icon.classList.toggle('fa-plus')
-  icon.classList.toggle('fa-minus')
+  render() {
+    return (
+      <div className="Country">
+        <h2>{this.props.translate(this.props.iso3166)}</h2>
 
-  const el = document.querySelector(`.${container}`)
-  el.classList.toggle('collapsed')
-}
+        {this.props.countryData &&
+        this.props.countryData.country_champions &&
+        this.props.countryData.country_champions.hits.hits.length > 0 &&
+        <div className="countryChampion">
+          <h3
+            onKeyDown={triggerClick}
+            tabIndex="0"
+            role="button"
+            onClick={() => this.setState({showCountryChampion:!this.state.showCountryChampion})}
+          >
+            <span>Country Champion</span> <i className="fa fa-minus" />
+          </h3>
 
-const Country = ({iso3166, countryData, translate}) => (
-
-  <div className="Country">
-    <h2>{translate(iso3166)}</h2>
-
-    {countryData &&
-    countryData.country_champions &&
-    countryData.country_champions.hits.hits.length > 0 &&
-    <div className="countryChampion">
-      <h3
-        onKeyDown={triggerClick}
-        tabIndex="0"
-        role="button"
-        onClick={(e) => hideContainer(e,'countryChampionContainer')}
-      >
-        <span>Country Champion</span> <i className="fa fa-minus" />
-      </h3>
-
-      <div className="countryChampionContainer">
-        {countryData.country_champions.hits.hits.map(champion => (
-          <div className="user" key={champion._source.about['@id']}>
-            {champion._source.about.image ? (
-              <Link to={`/resource/${champion._source.about['@id']}`}>
-                <div className="frame">
-                  <img
-                    src={champion._source.about.image}
-                    alt={translate(champion._source.about.name)}
-                    onLoad={e => {
-                      if (e.target.complete) {
-                        e.target.classList.add('visible')
-                      }}}
-                  />
-                  <Icon type={champion._type} />
+          <div className={`countryChampionContainer ${this.state.showCountryChampion ? '' : 'collapsed'}`}>
+            {this.props.countryData.country_champions.hits.hits.map(champion => (
+              <div className="user" key={champion._source.about['@id']}>
+                {champion._source.about.image ? (
+                  <Link to={`/resource/${champion._source.about['@id']}`}>
+                    <div className="frame">
+                      <img
+                        src={champion._source.about.image}
+                        alt={this.props.translate(champion._source.about.name)}
+                        onLoad={e => {
+                          if (e.target.complete) {
+                            e.target.classList.add('visible')
+                          }}}
+                      />
+                      <Icon type={champion._type} />
+                    </div>
+                  </Link>
+                ) :(
+                  <div className="frame">
+                    <Icon type={champion._type} />
+                  </div>
+                )
+                }
+                <div className="text">
+                  <Link to={`/resource/${champion._source.about['@id']}`}>
+                    {this.props.translate(champion._source.about.name)}
+                  </Link>
+                  <br />
+                  <Link to={`/resource/${champion._source.about['@id']}`}>
+                    {champion._source.about.email}
+                  </Link>
                 </div>
-              </Link>
-            ) :(
-              <div className="frame">
-                <Icon type={champion._type} />
               </div>
-            )
-            }
-            <div className="text">
-              <Link to={`/resource/${champion._source.about['@id']}`}>
-                {translate(champion._source.about.name)}
-              </Link>
-              <br />
-              <Link to={`/resource/${champion._source.about['@id']}`}>
-                {champion._source.about.email}
-              </Link>
+            ))}
+          </div>
+        </div>
+        }
+
+        {this.props.countryData &&
+        this.props.countryData.reports &&
+        this.props.countryData.reports.doc_count > 0 &&
+          <div className="countryReports">
+            <h3
+              onKeyDown={triggerClick}
+              tabIndex="0"
+              role="button"
+              onClick={() => this.setState({showReports:!this.state.showReports})}
+            >
+              <span>Country Reports</span> <i className="fa fa-minus" />
+            </h3>
+
+            <div className={`resourcesContainer ${this.state.showReports ? '' : 'collapsed'}`}>
+              {this.props.countryData.reports.country_reports.hits.hits.map(report => (
+                <div className="resource">
+                  <i className="fa fa-book" />
+                  <div className="text">
+                    <Link to={`/resource/${report._source.about['@id']}`}>
+                      {report._source.about.name}
+                    </Link>
+                  </div>
+                </div>
+              ))}
+
             </div>
           </div>
-        ))}
-      </div>
-    </div>
-    }
+        }
 
-    {countryData &&
-    countryData.reports &&
-    countryData.reports.doc_count > 0 &&
-      <div className="countryReports">
-        <h3
-          onKeyDown={triggerClick}
-          tabIndex="0"
-          role="button"
-          onClick={(e) => hideContainer(e,'resourcesContainer')}
-        >
-          <span>Country Reports</span> <i className="fa fa-minus" />
-        </h3>
-        <div className="resourcesContainer">
+        {this.props.countryData &&
+        this.props.countryData.by_type &&
+        this.props.countryData.by_type.buckets &&
+        <div className="statistics">
+          <h3
+            onKeyDown={triggerClick}
+            tabIndex="0"
+            role="button"
+            onClick={() => this.setState({showStatistics:!this.state.showStatistics})}
+          >
+            <span>Statistics</span> <i className="fa fa-minus" />
+          </h3>
+          <div className={`statisticsContainer ${this.state.showStatistics ? '' : 'collapsed'}`}>
+            <table>
+              <tbody>
 
-          {countryData.reports.country_reports.hits.hits.map(report => (
-            <div className="resource">
-              <i className="fa fa-book" />
-              <div className="text">
-                <Link to={`/r   esource/${report._source.about['@id']}`}>
-                  {report._source.about.name}
-                </Link>
-              </div>
-            </div>
-          ))}
-
+                {this.props.countryData.by_type.buckets.map(bucket => (
+                  <tr key={bucket.key}>
+                    <td>{bucket.doc_count}</td>
+                    <td>
+                      <Icon type={bucket.key} />
+                      &nbsp;<Link to={`/country/${this.props.iso3166.toLowerCase()}?filter.about.@type=${bucket.key}`}>{bucket.key}</Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
+        }
       </div>
-    }
-
-    {countryData &&
-    countryData.by_type &&
-    countryData.by_type.buckets &&
-    <div className="statistics">
-      <h3
-        onKeyDown={triggerClick}
-        tabIndex="0"
-        role="button"
-        onClick={(e) => hideContainer(e,'statisticsContainer')}
-      >
-        <span>Statistics</span> <i className="fa fa-minus" />
-      </h3>
-      <div className="statisticsContainer">
-        <table>
-          <tbody>
-
-            {countryData.by_type.buckets.map(bucket => (
-              <tr key={bucket.key}>
-                <td>{bucket.doc_count}</td>
-                <td>
-                  <Icon type={bucket.key} />
-                  &nbsp;<Link to={`/country/${countryData.key.toLowerCase()}?filter.about.@type=${bucket.key}`}>{bucket.key}</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-    }
-
-  </div>
-)
+    )}
+}
 
 Country.propTypes = {
   countryData: PropTypes.objectOf(PropTypes.any),
