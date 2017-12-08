f39dc778128cfc581fc1f1278233be5da1fc5025
Daniel Doblado <danieldoblado@gmail.com>
2017-12-08 13:39:00 +0100

    Add list to Action Buttons

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -5,30 +5,92 @@ import Link from './Link'
 
 import '../styles/ActionButtons.pcss'
 
-const ActionButtons = ({translate}) => (
-  <div className="ActionButtons">
-    <a href="#addEntry" title={translate('ActionButtons.addEntry')}>
-      <i className="fa fa-plus" />
-    </a>
-    <a href="/user/newsletter" title={translate('ActionButtons.subscribeToNewsletter')}>
-      <i className="fa fa-envelope" />
-    </a>
+class ActionButtons extends React.Component {
 
-    <a href="/aggregation/" title={translate('ActionButtons.statistics')}>
-      <i className="fa fa-pie-chart" />
-    </a>
+  constructor(props) {
+    super(props)
 
-    <a href="/feed/" title={translate('ActionButtons.recentAdditions')}>
-      <i className="fa fa-rss" />
-    </a>
-    <a href="#liked" title={translate('ActionButtons.liked')}>
-      <i className="fa fa-thumbs-up" />
-    </a>
-    <Link href="/resource/?filter.about.objectIn.@type=LighthouseAction&sort=like_count:DESC" title={translate('ActionButtons.lighthouse')}>
-      <i className="fa fa-lightbulb-o" />
-    </Link>
-  </div>
-)
+    this.state = {
+      showMenu: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="ActionButtons">
+        <button
+          title={this.props.translate('ActionButtons.addEntry')}
+          onClick={() => this.setState({showMenu:!this.state.showMenu})}
+        >
+          <i className="fa fa-plus" />
+
+          <ul className={this.state.showMenu ? '' : 'noDisplay'}>
+            <li>
+              <a href="#addOrganization">
+                + {this.props.translate('Organization')} <i className="fa fa-users" />
+              </a>
+            </li>
+
+            <li>
+              <a href="#addService">
+                + {this.props.translate('Service')} <i className="fa fa-desktop" />
+              </a>
+            </li>
+
+            <li>
+              <a href="#addProject">
+                + {this.props.translate('Project')} <i className="fa fa-gears" />
+              </a>
+            </li>
+
+            <li>
+              <a href="#addStory">
+                + {this.props.translate('Story')} <i className="fa fa-comment" />
+              </a>
+            </li>
+
+            <li>
+              <a href="#addEvent">
+                + {this.props.translate('Event')} <i className="fa fa-calendar" />
+              </a>
+            </li>
+
+            <li>
+              <a href="#addPublication">
+                + {this.props.translate('Publication')} <i className="fa fa-book" />
+              </a>
+            </li>
+
+            <li>
+              <a href="#addTool">
+                + {this.props.translate('Tool')} <i className="fa fa-folder" />
+              </a>
+            </li>
+
+          </ul>
+
+        </button>
+        <a href="/user/newsletter" title={translate('ActionButtons.subscribeToNewsletter')}>
+          <i className="fa fa-envelope" />
+        </a>
+
+        <a href="/aggregation/" title={translate('ActionButtons.statistics')}>
+          <i className="fa fa-pie-chart" />
+        </a>
+
+        <a href="/feed/" title={translate('ActionButtons.recentAdditions')}>
+          <i className="fa fa-rss" />
+        </a>
+        <a href="#liked" title={translate('ActionButtons.liked')}>
+          <i className="fa fa-thumbs-up" />
+        </a>
+        <Link href="/resource/?filter.about.objectIn.@type=LighthouseAction&sort=like_count:DESC" title={translate('ActionButtons.lighthouse')}>
+          <i className="fa fa-lightbulb-o" />
+        </Link>
+      </div>
+    )
+  }
+}
 
 ActionButtons.propTypes = {
   translate: PropTypes.func.isRequired
