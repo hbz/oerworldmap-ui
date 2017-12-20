bcb7f68ef794abe516c1a91dd68c47b359d5e3c4
Daniel Doblado <danieldoblado@gmail.com>
2017-12-20 12:26:56 +0100

    Change resources paths to assets

--- src/views/index.js
+++ src/views/index.js
@@ -8,11 +8,11 @@ export default ({ body, title, initialState, env }) => {
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Economica" rel="stylesheet">
-        <script src="/bundle.js" ></script>
+        <script src="/assets/bundle.js" ></script>
         <title>${title}</title>
         <script>window.__APP_INITIAL_STATE__ = ${initialState}</script>
-        <link rel="shortcut icon" href="/${favicon}" type="image/x-icon" />
-        ${env !== 'development' ? '<link rel="stylesheet" href="/styles.css">' : ''}
+        <link rel="shortcut icon" href="/assets/${favicon}" type="image/x-icon" />
+        ${env !== 'development' ? '<link rel="stylesheet" href="/assets/styles.css">' : ''}
       </head>
       <body>
         <div id="root"><div>${body}</div></div>
