6fdc7a4040d5c441318ae6dd2b4cae7d1724d2bd
Daniel Doblado <danieldoblado@gmail.com>
2017-12-22 16:21:23 +0100

    Remove toggle column from column

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,6 +1,3 @@
-/* global document */
-/* global window */
-
 import React from 'react'
 import PropTypes from 'prop-types'
 
@@ -9,41 +6,6 @@ const Column = ({ children, className }) => (
 
     <div className="columnContent">
       {children}
-    </div>
-
-    <div
-      role="button"
-      tabIndex="0"
-      className="toggleColumn"
-      onKeyDown={e => {
-        if (e.keyCode === 32) {
-          e.target.click()
-        }
-      }}
-      onClick={e => {
-        const column = e.target.parentElement.classList.contains('column')
-          ? e.target.parentElement
-          : e.target.parentElement.parentElement
-        column.parentElement.classList.toggle('hideColumns')
-
-        document.querySelector('.ColumnList').classList.remove('noDisplay')
-        document.querySelector('.PagedCollection').classList.remove('noDisplay')
-
-        window.setTimeout(()=> {
-
-          if (column.parentElement.classList.contains('hideColumns')) {
-            document.querySelector('.ColumnList').classList.add('noDisplay')
-            document.querySelector('.PagedCollection').classList.add('noDisplay')
-          } else {
-            document.querySelector('.ColumnList').classList.remove('noDisplay')
-            document.querySelector('.PagedCollection').classList.remove('noDisplay')
-          }
-        },1200)
-
-
-      }}
-    >
-      <i className="fa fa-chevron-right" />
     </div>
 
   </div>
