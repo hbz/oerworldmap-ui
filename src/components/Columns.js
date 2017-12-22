3318455aa7a28cefe2939af6da54be69145f1217
Daniel Doblado <danieldoblado@gmail.com>
2017-12-22 16:22:54 +0100

    Add logic for hidding columns

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,3 +1,4 @@
+/* global window */
 import React from 'react'
 import PropTypes from 'prop-types'
 
@@ -5,14 +6,55 @@ import withEmitter from './withEmitter'
 
 import '../styles/components/Columns.pcss'
 
-const Columns = ({ children }) => (
-  <aside className="Columns">
-    {children}
-  </aside>
-)
+class Columns extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showColumns: this.props.size ? true : false
+    }
+  }
+
+  componentDidMount() {
+    this.props.emitter.on('toggleColumns', () => {
+      this.setState({showColumns:!this.state.showColumns})
+
+      const ColumnList = this.columns.querySelector('.ColumnList')
+      const PagedCollection = this.columns.querySelector('.PagedCollection')
+
+      ColumnList.classList.remove('noDisplay')
+      PagedCollection.classList.remove('noDisplay')
+
+      window.setTimeout(()=> {
+        if (!this.state.showColumns) {
+          ColumnList.classList.add('noDisplay')
+          PagedCollection.classList.add('noDisplay')
+        } else {
+          ColumnList.classList.remove('noDisplay')
+          PagedCollection.classList.remove('noDisplay')
+        }
+      },1200)
+    })
+  }
+
+  render() {
+    return (
+      <aside
+        className={`Columns ${this.state.showColumns ? '' : 'hideColumns'}`}
+        ref={col => this.columns = col}
+      >
+        {this.props.children}
+      </aside>
+    )
+  }
+
+}
 
 Columns.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  size: PropTypes.number.isRequired,
+  emitter: PropTypes.objectOf(PropTypes.any).isRequired,
 }
 
 export default withEmitter(Columns)
