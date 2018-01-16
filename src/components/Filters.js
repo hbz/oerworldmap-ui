aee5cd7e7e89994f2b17c40af5f1cc4b6475d661
Daniel Doblado <danieldoblado@gmail.com>
2018-01-16 13:09:45 +0100

    Add expand and collapse filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -64,147 +64,185 @@ const secondaryFilters = [
   },
 ]
 
-const Filters = ({query, filters, aggregations, emitter, translate, member, size}) => {
+class Filters extends React.Component {
 
-  let sizes = [10,20,50,100,200]
+  constructor(props) {
+    super(props)
+    this.state = {
+      collapsed: !Object.keys(this.props.filters).some(v => secondaryFilters.map(f => f.name).includes(v))
+    }
 
-  if (!sizes.includes(size) && size > -1) {
-    sizes.push(size)
-    sizes = sizes.sort((a, b) => a - b)
+    this.sizes = [10,20,50,100,200]
+
+    if (!this.sizes.includes(this.props.size) && this.props.size > -1) {
+      this.sizes.push(this.props.size)
+      this.sizes = this.sizes.sort((a, b) => a - b)
+    }
   }
 
-  return (
-    <nav className="Filters">
+  render() {
+    return (
+      <nav className="Filters">
 
-      <form action="" onSubmit={(evt) => onSubmit(evt, emitter)}>
-        <div className="FiltersControls">
-          <div className="filterSearch">
-            <input type="search" name="q" defaultValue={query} placeholder={`${translate('Filters.searchTheMap')}...`} />
-            <i
-              className="fa fa-th-list"
-              title="Show List"
-              tabIndex="0"
-              role="button"
-              onClick={() => {emitter.emit('toggleColumns')}}
-              onKeyDown={e => {
-                if (e.keyCode === 32) {
-                  e.target.click()
+        <form action="" onSubmit={(evt) => onSubmit(evt, this.props.emitter)}>
+          <div className="FiltersControls">
+            <div className="filterSearch">
+              <input type="search" name="q" defaultValue={this.props.query} placeholder={`${this.props.translate('Filters.searchTheMap')}...`} />
+              <i
+                className="fa fa-th-list"
+                title="Show List"
+                tabIndex="0"
+                role="button"
+                onClick={() => {this.props.emitter.emit('toggleColumns')}}
+                onKeyDown={e => {
+                  if (e.keyCode === 32) {
+                    e.target.click()
+                  }
+                }}
+              />
+              <noscript>
+                <div className="search-bar">
+                  <input type="submit" className="btn" />
+                </div>
+              </noscript>
+            </div>
+
+            <div className="filterType primary">
+              {primaryFilters.map((filterDef) => {
+                const aggregation = this.props.aggregations[filterDef.name]
+                  && this.props.aggregations[filterDef.name].buckets.length
+                  ? this.props.aggregations[filterDef.name] : null
+                const filter = this.props.filters[filterDef.name] || []
+                if (!aggregation) {
+                  return
                 }
-              }}
-            />
-            <noscript>
-              <div className="search-bar">
-                <input type="submit" className="btn" />
-              </div>
-            </noscript>
-          </div>
+                switch(filterDef.type) {
+                case 'button':
+                  return (
+                    <ButtonFilter
+                      key={filterDef.name}
+                      aggregation={aggregation}
+                      filter={filter}
+                      submit={onSubmit}
+                    />
+                  )
+                case 'dropdown':
+                default:
+                  return (
+                    <DropdownFilter
+                      key={filterDef.name}
+                      icon={filterDef.icon}
+                      aggregations={aggregation}
+                      filters={filter}
+                      filterName={`filter.${filterDef.name}`}
+                      submit={onSubmit}
+                    />
+                  )
+                }
+              })}
 
-          <div className="filterType primary">
-            {primaryFilters.map((filterDef) => {
-              const aggregation = aggregations[filterDef.name]
-                && aggregations[filterDef.name].buckets.length
-                ? aggregations[filterDef.name] : null
-              const filter = filters[filterDef.name] || []
-              if (!aggregation) {
-                return
-              }
-              switch(filterDef.type) {
-              case 'button':
-                return (
-                  <ButtonFilter
-                    key={filterDef.name}
-                    aggregation={aggregation}
-                    filter={filter}
-                    submit={onSubmit}
-                  />
-                )
-              case 'dropdown':
-              default:
-                return (
-                  <DropdownFilter
-                    key={filterDef.name}
-                    icon={filterDef.icon}
-                    aggregations={aggregation}
-                    filters={filter}
-                    filterName={`filter.${filterDef.name}`}
-                    submit={onSubmit}
-                  />
-                )
-              }
-            })}
+            </div>
 
-          </div>
-
-          <div className="filterType secondary">
-            {secondaryFilters.map((filterDef) => {
-              const aggregation = aggregations[filterDef.name]
-                && aggregations[filterDef.name].buckets.length
-                ? aggregations[filterDef.name] : null
-              const filter = filters[filterDef.name] || []
-              if (!aggregation) {
-                return
-              }
-              switch(filterDef.type) {
-              case 'button':
-                return (
-                  <ButtonFilter
-                    key={filterDef.name}
-                    aggregation={aggregation}
-                    filter={filter}
-                    submit={onSubmit}
-                  />
-                )
-              case 'dropdown':
-              default:
-                return (
-                  <DropdownFilter
-                    key={filterDef.name}
-                    icon={filterDef.icon}
-                    aggregations={aggregation}
-                    filters={filter}
-                    filterName={`filter.${filterDef.name}`}
-                    submit={onSubmit}
-                  />
-                )
-              }
-            })}
-
-          </div>
-
-          <div className="clearFilter">
-            <Link href="/resource/">{translate('Filters.clearFilters')}</Link>
-          </div>
-        </div>
-
-        <div className="sortContainer">
-          <PagedCollection size={size} member={member}>
-            <select
-              name="sort"
-              className="styledSelect"
-              style={{width: (translate('Filters.relevance').length * 8)+15}}
-              onChange={(evt) => {
-                evt.target.style.width = (evt.target.options[evt.target.selectedIndex].text.length * 8) + 15 + 'px'
-                onSubmit(evt, emitter)
-              }}
+            <div
+              className={`filterType secondary${this.state.collapsed ? ' collapsed' : ''}`}
             >
-              <option value="">{translate('Filters.relevance')}</option>
-              <option value="dateCreated:ASC">{translate('Filters.dateCreated')}</option>
-            </select>
+              {secondaryFilters.map((filterDef) => {
+                const aggregation = this.props.aggregations[filterDef.name]
+                  && this.props.aggregations[filterDef.name].buckets.length
+                  ? this.props.aggregations[filterDef.name] : null
+                const filter = this.props.filters[filterDef.name] || []
+                if (!aggregation) {
+                  return
+                }
+                switch(filterDef.type) {
+                case 'button':
+                  return (
+                    <ButtonFilter
+                      key={filterDef.name}
+                      aggregation={aggregation}
+                      filter={filter}
+                      submit={onSubmit}
+                    />
+                  )
+                case 'dropdown':
+                default:
+                  return (
+                    <DropdownFilter
+                      key={filterDef.name}
+                      icon={filterDef.icon}
+                      aggregations={aggregation}
+                      filters={filter}
+                      filterName={`filter.${filterDef.name}`}
+                      submit={onSubmit}
+                    />
+                  )
+                }
+              })}
 
-            <select onChange={e => onSubmit(e, emitter)} className="btn" name="size" value={size}>
-              {sizes.map(number => (
-                number >= 0 &&
-                  <option key={number} value={number}>{number}</option>
-              ))}
-              <option value="-1">All</option>
-            </select>
-          </PagedCollection>
-        </div>
+            </div>
 
-      </form>
+            <div className="filtersControls">
+              {this.state.collapsed ?
+                (
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault()
+                      this.setState({collapsed:false})
+                    }}
+                  >
+                    Show more filter
+                  </button>
+                ) :
+                (
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault()
+                      this.setState({collapsed:true})
+                    }}
+                  >
+                    Hide extended filter
+                  </button>
+                )
+              }
 
-    </nav>
-  )
+              <div className="clearFilter">
+                <Link href="/resource/">{this.props.translate('Filters.clearFilters')}</Link>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className="sortContainer">
+            <PagedCollection size={this.props.size} member={this.props.member}>
+              <select
+                name="sort"
+                className="styledSelect"
+                style={{width: (this.props.translate('Filters.relevance').length * 8)+15}}
+                onChange={(evt) => {
+                  evt.target.style.width = (evt.target.options[evt.target.selectedIndex].text.length * 8) + 15 + 'px'
+                  onSubmit(evt, this.props.emitter)
+                }}
+              >
+                <option value="">{this.props.translate('Filters.relevance')}</option>
+                <option value="dateCreated:ASC">{this.props.translate('Filters.dateCreated')}</option>
+              </select>
+
+              <select onChange={e => onSubmit(e, this.props.emitter)} className="btn" name="size" value={this.props.size}>
+                {this.sizes.map(number => (
+                  number >= 0 &&
+                    <option key={number} value={number}>{number}</option>
+                ))}
+                <option value="-1">All</option>
+              </select>
+            </PagedCollection>
+          </div>
+
+        </form>
+
+      </nav>
+    )
+  }
 }
 
 Filters.propTypes = {
