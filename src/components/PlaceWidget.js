14f958509537f2c896e2be717e6194555870f51d
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-01-26 18:15:59 +0100

    Reuse Checkbox component in PlaceWidget

--- src/components/PlaceWidget.js
+++ src/components/PlaceWidget.js
@@ -1,76 +1,37 @@
 import React from 'react'
 
-import { Fieldset } from 'json-pointer-form'
-
-import { debounce } from '../common'
+import { Fieldset, Checkbox } from 'json-pointer-form'
 
 class PlaceWidget extends Fieldset {
 
   constructor(props) {
     super(props)
-    this.state.search = ""
-    this.state.suggestions = []
-    this.handleFilterChange = this.handleFilterChange.bind(this)
     this.select = this.select.bind(this)
-  }
-
-  componentWillMount() {
-    this.delayedCallback = debounce(function(event) {
-      this.setState({search: event.target.value})
-      if (event.target.value) {
-        this.props.getOptions(event.target.value, {properties: {mapzen: true}}, (result) => {
-          this.setState( { suggestions: result.member.map(member => member.about) } )
-        })
-      } else {
-        this.setState( { suggestions: [] } )
-      }
-    }.bind(this), 500)
-  }
-
-  handleFilterChange(event) {
-    event.persist()
-    this.setState({search: event.target.value})
-    this.delayedCallback(event)
   }
 
   select(suggestion) {
     this.props.handleChange({
       target: {
         name: this.props.name,
-        value: suggestion,
-        type: "custom"
+        value: suggestion.target.value,
+        type: 'custom'
       }
     })
-    this.setState( { search: '', suggestions: [] } )
   }
 
   render() {
     return (
       <div>
-        <div className="filterContainer">
-          <input
-            type="text"
-            className="filter"
-            onChange={this.handleFilterChange}
-            value={this.state.search}
-          />
-          <ul>
-            {this.state.suggestions.map(suggestion => (
-              <li key={suggestion['@id']}>
-                <input
-                  name={this.props.name}
-                  type="checkbox"
-                  value={suggestion['@id']}
-                  id={suggestion['@id']}
-                  onChange={() => this.select(suggestion)}
-                />
-                <label htmlFor={suggestion['@id']}>
-                  {this.props.getLabel(suggestion)}
-                </label>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Checkbox
+          errors={[]}
+          getLabel={this.props.getLabel}
+          getOptions={this.props.getOptions}
+          handleChange={(suggestion) => this.select(suggestion)}
+          name={this.props.name}
+          parents={this.props.parents}
+          property={this.props.property}
+          schema={{properties: {mapzen: true}}}
+        />
         {this.getChildren()}
       </div>
     )
