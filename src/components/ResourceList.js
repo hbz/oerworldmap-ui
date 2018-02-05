e793442674c165c9768746b1458e5cb575bb7f52
Daniel Doblado <danieldoblado@gmail.com>
2018-02-05 12:40:10 +0100

    Add ResourceList component

---
+++ src/components/ResourceList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import withI18n from './withI18n'
+import Link from './Link'
+import Icon from './Icon'
+
+const ResourceList = ({ translate, title, list }) => (
+  <div className="ResourceList asideList">
+    <h3>{translate(title)}</h3>
+    <ul>
+      {Array.isArray(list) && list.map(item => (
+        <li>
+          <Icon type={item['@type']} /> <Link href={`/resource/${item['@id']}`}>{translate(item.name)}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+export default withI18n(ResourceList)
