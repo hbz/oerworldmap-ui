49132a09dae6c03e055c4807af220db58eaa1d95
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-02-13 18:12:27 +0100

    Extract SocialLinks component

--- src/components/WebPageView.js
+++ src/components/WebPageView.js
@@ -8,6 +8,7 @@ import ItemList from './ItemList'
 import Link from './Link'
 import ConceptTree from './ConceptTree'
 import WebPageUserActions from './WebPageUserActions'
+import SocialLinks from './SocialLinks'
 
 import { formatURL/*, obfuscate*/ } from '../common'
 import '../styles/components/WebPageView.pcss'
@@ -22,33 +23,7 @@ const WebPageView = ({translate, moment, about, user, view}) => {
     <div className="WebPageView">
 
       {about.sameAs &&
-        <div className="social">
-          {about.sameAs.map(link => {
-
-            let icon = 'external-link-square'
-
-            if (link.includes('facebook')) {
-              icon = 'facebook'
-            } else if (link.includes('twitter')) {
-              icon = 'twitter'
-            } else if (link.includes('linkedin')) {
-              icon = 'linkedin'
-            } else if (link.includes('instagram')) {
-              icon = 'instagram'
-            } else if (link.includes('plus')) {
-              icon = 'plus'
-            } else if (link.includes('youtube')) {
-              icon = 'youtube'
-            }
-
-            return (
-              <a key={link} href={link} rel="noopener" target="_blank">
-                <i className={`fa fa-${icon}`} />
-              </a>
-            )
-          })
-          }
-        </div>
+        <SocialLinks links={about.sameAs} />
       }
 
       {about.countryChampionFor &&
