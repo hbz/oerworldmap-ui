4b3a8bf73f36812ce2e6cb1a8866e780516313e2
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-02-15 23:08:03 +0100

    Make place widget collapsible

--- src/components/PlaceWidget.js
+++ src/components/PlaceWidget.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import pointer from 'json-pointer'
 import { Fieldset, Checkbox } from 'json-pointer-form'
 
 class PlaceWidget extends Fieldset {
@@ -7,6 +8,7 @@ class PlaceWidget extends Fieldset {
   constructor(props) {
     super(props)
     this.select = this.select.bind(this)
+    this.state.collapsed = props.collapsed
   }
 
   select(suggestion) {
@@ -20,23 +22,42 @@ class PlaceWidget extends Fieldset {
   }
 
   render() {
+    const name = pointer.compile(this.getParentPath())
     return (
-      <div>
-        <Checkbox
-          errors={[]}
-          getLabel={this.props.getLabel}
-          getOptions={this.props.getOptions}
-          handleChange={(suggestion) => this.select(suggestion)}
-          name={this.props.name}
-          parents={this.props.parents}
-          property={this.props.property}
-          schema={{properties: {mapzen: true}}}
-        />
-        {this.getChildren()}
-      </div>
+      <fieldset className={`${this.props.property} ${this.props.className || ''}`}>
+        <legend className={this.props.errors[name] ? "error" : "title"}>
+          {this.props.errors[name]
+            ? this.props.getLabel(this.props.errors[name])
+            : this.props.getLabel(this.props.title || this.props.property)}
+        </legend>
+        <p className={this.state.collapsed ? null : 'hidden'}>
+          <em>{this.props.getLabel(this.props.description)}</em>
+          <button className="btn btn-default" type="button" onClick={() => this.setState({collapsed: false})}>
+            {this.props.getLabel('Place.show')}
+          </button>
+        </p>
+        <div className={this.state.collapsed ? 'hidden' : null}>
+          <Checkbox
+            errors={[]}
+            getLabel={this.props.getLabel}
+            getOptions={this.props.getOptions}
+            handleChange={(suggestion) => this.select(suggestion)}
+            name={this.props.name}
+            parents={this.props.parents}
+            property={this.props.property}
+            placeholder={this.props.getLabel('Foobar')}
+            schema={{properties: {mapzen: true}}}
+          />
+          {this.getChildren()}
+        </div>
+      </fieldset>
     )
   }
 
+}
+
+PlaceWidget.defaultProps = {
+  collapsed: false
 }
 
 export default PlaceWidget
