89e5db0ddfe1ead4e649af5e886a4fb5ecc101e7
Daniel Doblado <danieldoblado@gmail.com>
2018-02-23 15:50:06 +0100

    Change variable name

--- src/components/WebPageView.js
+++ src/components/WebPageView.js
@@ -267,7 +267,7 @@ const WebPageView = ({translate, moment, about, user, view, expandAll}) => {
           }
 
           {about.availableChannel &&
-          about.availableChannel.some(grant => grant.availableLanguage) &&
+          about.availableChannel.some(channel => channel.availableLanguage) &&
             <Block title={translate(`${about['@type']}.availableChannel.availableLanguage`)}>
               <ul className="commaSeparatedList">
                 {[].concat.apply([], about.availableChannel.map(channel => channel.availableLanguage)).map(lang => (
