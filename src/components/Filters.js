b8f95c1468081bbfc1dbc672b9d0abd5079a83cb
Daniel Doblado <danieldoblado@gmail.com>
2018-03-06 10:23:52 +0100

    Set default size for list to 20

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -126,7 +126,7 @@ class Filters extends React.Component {
       )
     }
 
-    this.sizes = [10,20,50,100,200]
+    this.sizes = [20,50,100,200]
 
     if (!this.sizes.includes(this.props.size) && this.props.size > -1) {
       this.sizes.push(this.props.size)
