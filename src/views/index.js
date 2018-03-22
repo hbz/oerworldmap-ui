9aab7309127ec29837c73fafffeb823e3da171d0
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-03-22 16:33:04 +0100

    Extract piwik template

--- src/views/index.js
+++ src/views/index.js
@@ -1,26 +1,11 @@
 import favicon from '../assets/favicon.png'
+import piwik from './piwik'
 
-export default ({ body, title, initialState, env, piwikConfig}) => {
+export default ({body, title, initialState, env, piwikConfig}) => {
 
-  let piwik = ''
-  if (env === 'production' && (piwikConfig.id && piwikConfig.url)) {
-    piwik = `<script type="text/javascript">
-    var _paq = _paq || [];
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-    var u='//${piwikConfig.url}';
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', ${piwikConfig.id}]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-    })();
-    </script>
-    <noscript>
-      <p><img src="//${piwikConfig.url}piwik.php?idsite=${piwikConfig.id}" style="border:0;" alt="" /></p>
-    </noscript>
-    `
-  }
+  const piwikEmbed = true || env === 'production' && (piwikConfig.id && piwikConfig.url)
+    ? piwik(piwikConfig)
+    : ''
 
   return `
     <!doctype html>
@@ -31,12 +16,12 @@ export default ({ body, title, initialState, env, piwikConfig}) => {
         <script src="/assets/bundle.js" ></script>
         <title>${title}</title>
         <script>window.__APP_INITIAL_STATE__ = ${initialState}</script>
-        ${piwik}
         <link rel="shortcut icon" href="/assets/${favicon}" type="image/x-icon" />
         ${env !== 'development' ? '<link rel="stylesheet" href="/assets/styles.css">' : ''}
       </head>
       <body>
         <div id="root"><div>${body}</div></div>
+        ${piwikEmbed}
       </body>
     </html>
   `
