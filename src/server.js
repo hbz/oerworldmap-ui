e8ff585ca3f02e767d81d7ce5f19ab240898dbec
Daniel Doblado <danieldoblado@gmail.com>
2018-04-30 15:26:06 +0200

    Add dynamic language tag and text direction to header

--- src/server.js
+++ src/server.js
@@ -130,7 +130,8 @@ server.get(/^(.*)$/, (req, res) => {
         .replace(/\u2028/g, "\\u2028").replace(/\u2029/g, "\\u2029"),
       title,
       piwikConfig,
-      embed
+      embed,
+      locales
     }))
   })
 })
