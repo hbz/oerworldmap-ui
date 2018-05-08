11ebc4ca501fe86a25b722204c1711c832fde90a
Daniel Doblado <danieldoblado@gmail.com>
2018-05-08 10:33:40 +0200

    Add log indicating server rendering

--- src/server.js
+++ src/server.js
@@ -118,6 +118,7 @@ server.get(/^(.*)$/, (req, res) => {
   const context = { locales, authorization, user, mapboxConfig, phrases, apiConfig, schema, embed }
   //TODO: use actual request method
   router(api).route(req.path, context).get(req.query).then(({title, data, render, err}) => {
+    console.info('Render from Server:', req.url)
     res.send(template({
       env: process.env.NODE_ENV,
       body: renderToString(render(data)),
