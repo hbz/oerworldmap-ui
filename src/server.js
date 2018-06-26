bfea2c18e920190a5f9244441680dce2bffc370d
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-06-26 13:22:45 +0200

    Extract i18n string from vocabs

--- src/server.js
+++ src/server.js
@@ -67,6 +67,16 @@ server.use((req, res, next) => {
 const supportedLanguages = i18nConfig.supportedLanguages.trim().split(/\s+/)
 const defaultLanguage = i18nConfig.defaultLanguage
 const bundles = ['ui', 'iso3166-1-alpha-2', 'iso639-1', 'iso3166-2', 'labels', 'descriptions']
+const vocabs = [
+  'esc', 'isced-1997', 'licenses', 'organizations', 'persons', 'projects', 'publications',
+  'sectors', 'services'
+]
+const extractLabels = (concepts, language) => concepts.reduce((acc, cur) => {
+  const name = cur.name.find(name => name['@language'] === language)
+  name && (acc[cur['@id']] = name['@value'])
+  cur.narrower && Object.assign(acc, extractLabels(cur.narrower, language))
+  return acc
+}, {})
 const i18ns = {}
 supportedLanguages.map(language => {
   const i18n = {}
@@ -82,6 +92,9 @@ supportedLanguages.map(language => {
       Object.assign(i18n, obj)
     }
   })
+  vocabs.forEach(vocab =>
+    Object.assign(i18n, extractLabels(require(`./json/${vocab}.json`).hasTopConcept, language))
+  )
   i18ns[language] = i18n
 })
 
