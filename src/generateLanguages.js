eae558102493e6676a4d10afe069a5e80de6f408
Felix Ostrowski <felix.ostrowski@googlemail.com>
2018-06-27 14:03:05 +0200

    Use default language as fallback for missing i18n

--- src/generateLanguages.js
+++ src/generateLanguages.js
@@ -1,20 +1,42 @@
-import { existsSync, readFileSync } from 'fs'
 import { i18nConfig } from '../config'
 
 const supportedLanguages = i18nConfig.supportedLanguages.trim().split(/\s+/)
-// const defaultLanguage = i18nConfig.defaultLanguage
+const defaultLanguage = i18nConfig.defaultLanguage
 const bundles = ['ui', 'iso3166-1-alpha-2', 'iso639-1', 'iso3166-2', 'labels', 'descriptions']
 const i18ns = {}
-supportedLanguages.map(language => {
-  const i18n = {}
+i18ns[defaultLanguage] = {
+  descriptions: {}
+}
+
+bundles.forEach(bundle => {
+  const obj = {}
+  try {
+    Object.assign(obj, require(`../docs/_data/locale/${bundle}.json`))
+  } catch (e) {
+    console.error("Missing i18n file ${bundle}.json")
+    return
+  }
+  //FIXME: special case descriptions, refactor so that all l10ns are segmented by bundle name
+  if (bundle === 'descriptions') {
+    Object.assign(i18ns[defaultLanguage]['descriptions'], obj)
+  } else {
+    Object.assign(i18ns[defaultLanguage], obj)
+  }
+})
+
+supportedLanguages.filter(language => language !== defaultLanguage).forEach(language => {
+  const i18n = JSON.parse(JSON.stringify(i18ns[defaultLanguage]))
   bundles.forEach(bundle => {
-    const i18nfile = existsSync(`./docs/_data/locale/${bundle}_${language}.json`)
-      ? `./docs/_data/locale/${bundle}_${language}.json`
-      : `./docs/_data/locale/${bundle}.json`
-    const obj = JSON.parse(readFileSync(i18nfile, 'utf8'))
+    const obj = {}
+    try {
+      Object.assign(obj, require(`../docs/_data/locale/${bundle}_${language}.json`))
+    } catch (e) {
+      console.error("Missing i18n file ${bundle}.json")
+      return
+    }
     //FIXME: special case descriptions, refactor so that all l10ns are segmented by bundle name
     if (bundle === 'descriptions') {
-      i18n['descriptions'] = obj
+      Object.assign(i18n['descriptions'], obj)
     } else {
       Object.assign(i18n, obj)
     }
@@ -23,4 +45,3 @@ supportedLanguages.map(language => {
 })
 
 export default i18ns
-
