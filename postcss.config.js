7c3ecdc86b6c8a02962625e18d39d893268cff5d
Daniel Doblado <danieldoblado@gmail.com>
2018-06-27 17:12:08 +0200

    Enable PostCSS features and use the new naming convetion

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,9 @@ module.exports = {
     'postcss-preset-env': {
       browsers: 'last 2 versions',
       features: {
-        'nesting-rules': true
+        'nesting-rules': true,
+        'custom-media-queries': true,
+        'color-mod-function': true
       }
     },
   },
