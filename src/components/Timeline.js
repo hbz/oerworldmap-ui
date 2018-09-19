69c86fe145d186ab4b8122c7e5b90efc47862167
Daniel Doblado <danieldoblado@gmail.com>
2018-09-19 10:05:59 +0200

    Intermin commit

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -9,6 +9,29 @@ const Timeline = ({translate}) => (
     <div className="timelineContainer">
       <section className="timelineContainerMain">
 
+
+        <div className="timelineBlock prominent">
+          <div className="timelineBlockMetadata">
+            <div className="timelineBlockMetadataTime">
+              10. Januar 2018
+            </div>
+            <div className="timelineBlockMetadataActions">
+              <i className="fa fa-close" />
+            </div>
+          </div>
+
+          <div className="timelineBlockTitle">
+            Stay tunned: Next week we launch owr new UI
+          </div>
+
+          <div className="timelineBlockContent">
+            With the new UI we launch a bunch o new features, like the history of an entry, a new single resource view a new landing page and so on. Read on
+            <br />
+            <br />
+            http://oerworldmap.org/resource/urn%3Auuid%3A55d772c7-4059-456d-a49d-40e2ecb48a10
+          </div>
+        </div>
+
         <div className="timelineBlock">
           <div className="timelineBlockMetadata">
             <div className="timelineBlockMetadataTime">
@@ -21,8 +44,9 @@ const Timeline = ({translate}) => (
 
           <div className="timelineBlockTitle">
             <Link href="/resource/blabla">Karin Driesen</Link>
-            &nbsp;Edited&nbsp;
+            &nbsp;edited&nbsp;
             <Link href="/resource/blabla">OER World Map</Link>
+            The following
           </div>
 
           <div className="timelineBlockContent">
@@ -42,7 +66,7 @@ const Timeline = ({translate}) => (
 
           <div className="timelineBlockTitle">
             <Link href="/resource/blabla">Karin Driesen</Link>
-            &nbsp;Edited&nbsp;
+            &nbsp;edited&nbsp;
             <Link href="/resource/blabla">OER World Map</Link>
           </div>
 
@@ -54,7 +78,15 @@ const Timeline = ({translate}) => (
       </section>
 
       <aside className="timelineContainerAside">
-        aside
+        <div className="timelineBlock">
+
+          <div className="userBlock">
+            <img src="https://randomuser.me/api/portraits/women/44.jpg" alt="Frau" />
+            <h2>Marta Mustermann</h2>
+            <h3>Country Champion for <Link href="#none">United Kingdom</Link></h3>
+          </div>
+
+        </div>
       </aside>
 
     </div>
