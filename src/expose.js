9b6260f2a908d8f8107d96c2b6f0ddf27ac7bf12
Felix Ostrowski <felix.ostrowski@googlemail.com>
2019-02-22 16:53:42 +0100

    Fix expose rules

--- src/expose.js
+++ src/expose.js
@@ -1,10 +1,10 @@
 export default (action, user, context) => {
   switch (action) {
   case 'deleteEntry':
-    return context['@id'] && user && user.groups.includes('admin')
+    return context['@id'] && user && user.groups && user.groups.includes('admin')
   case 'editEntry':
-    return user && user.groups
-      && (user.groups.includes('admin')
+    return user
+      && (user.groups && user.groups.includes('admin')
         || (context['@type'] !== 'Person' || context['@id'] === user.id)
       )
   case 'logEntry':
