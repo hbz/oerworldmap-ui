5f54b04d97518f37b2326970b135b774a77a7523
Daniel Doblado <danieldoblado@gmail.com>
2019-04-09 10:14:17 +0200

    Comment logic for data-inject-newWindowLink since is not in oerpolicies.md anymore

--- src/site.js
+++ src/site.js
@@ -178,77 +178,77 @@ const createAccordeon = (() => {
 const createKibanaListener = (() => {
   const init = () => {
 
-    const newWindowLink = document.querySelector('[data-inject-newWindowLink]')
+    // const newWindowLink = document.querySelector('[data-inject-newWindowLink]')
 
-    newWindowLink.addEventListener("click", (e) => {
-      e.preventDefault()
+    // newWindowLink.addEventListener("click", (e) => {
+    //   e.preventDefault()
 
-      const documentBody = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>Document</title>
-        </head>
-        <body>
-          <iframe
-            src="/kibana/app/kibana#/dashboard/3f24aa90-e370-11e8-bc1a-bd36147d8400?embed=true&_g=()"
-            height="750"
-            width="800"
-            style="border:0; width: 100%; margin: 0 auto;"
-            data-scope="filter.about.@type=Policy"
-          >
-          </iframe>
-          <script>
+    //   const documentBody = `
+    //     <!DOCTYPE html>
+    //     <html lang="en">
+    //     <head>
+    //       <meta charset="UTF-8">
+    //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    //       <title>Document</title>
+    //     </head>
+    //     <body>
+    //       <iframe
+    //         src="/kibana/app/kibana#/dashboard/3f24aa90-e370-11e8-bc1a-bd36147d8400?embed=true&_g=()"
+    //         height="750"
+    //         width="800"
+    //         style="border:0; width: 100%; margin: 0 auto;"
+    //         data-scope="filter.about.@type=Policy"
+    //       >
+    //       </iframe>
+    //       <script>
 
-          const getURL = (route) => {
-            let url = route.path
-            let params = []
-            for (const param in route.params) {
-              const value = route.params[param]
-              if (Array.isArray(value)) {
-                value && (params = params.concat(value.map(value => param + '=' + encodeURIComponent(value))))
-              } else {
-                value && params.push(param + '=' + encodeURIComponent(value))
-              }
-            }
-            if (params) {
-              url += '?' + params.join('&')
-            }
-            if (route.hash) {
-              url += '#' + route.hash
-            }
-            return url
-          }
+    //       const getURL = (route) => {
+    //         let url = route.path
+    //         let params = []
+    //         for (const param in route.params) {
+    //           const value = route.params[param]
+    //           if (Array.isArray(value)) {
+    //             value && (params = params.concat(value.map(value => param + '=' + encodeURIComponent(value))))
+    //           } else {
+    //             value && params.push(param + '=' + encodeURIComponent(value))
+    //           }
+    //         }
+    //         if (params) {
+    //           url += '?' + params.join('&')
+    //         }
+    //         if (route.hash) {
+    //           url += '#' + route.hash
+    //         }
+    //         return url
+    //       }
 
-          window.addEventListener("message", (msg) => {
+    //       window.addEventListener("message", (msg) => {
 
-            if (msg.data.filter && msg.data.key) {
+    //         if (msg.data.filter && msg.data.key) {
 
-              const iframe = document.querySelector('iframe')
-              const { scope } = iframe && iframe.dataset
+    //           const iframe = document.querySelector('iframe')
+    //           const { scope } = iframe && iframe.dataset
 
-              const info = {
-                filter: msg.data.filter,
-                key: msg.data.key,
-                scope
-              }
-              window.opener.postMessage(info, "*")
-            }
+    //           const info = {
+    //             filter: msg.data.filter,
+    //             key: msg.data.key,
+    //             scope
+    //           }
+    //           window.opener.postMessage(info, "*")
+    //         }
 
-          })
+    //       })
 
-          </script>
-        </body>
-        </html>
-      `
-      const options = "menubar=no,location=no,resizable=yes,scrollbars=yes,status=yes,width=800,height=750"
-      const newWindow = window.open("", 'OER Policies', options)
-      newWindow.document.write(documentBody)
-      newWindow.document.close()
-    })
+    //       </script>
+    //     </body>
+    //     </html>
+    //   `
+    //   const options = "menubar=no,location=no,resizable=yes,scrollbars=yes,status=yes,width=800,height=750"
+    //   const newWindow = window.open("", 'OER Policies', options)
+    //   newWindow.document.write(documentBody)
+    //   newWindow.document.close()
+    // })
 
     window.addEventListener("message", (msg) => {
 
