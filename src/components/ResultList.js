e44b590a37fae4944b97c8c3862ebd7e857db6e8
Daniel Doblado <danieldoblado@gmail.com>
2019-04-11 15:10:14 +0200

    Add icon, move location and minor fixes for spacing

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -5,6 +5,7 @@ import removeMd from 'remove-markdown'
 import withI18n from './withI18n'
 import withEmitter from './withEmitter'
 import Link from './Link'
+import Icon from './Icon'
 
 import '../styles/components/ResultList.pcss'
 
@@ -21,30 +22,9 @@ const ResultList = ({ translate, emitter, listItems }) => {
             emitter.emit('hoverPoint', { id: '' })
           }}
         >
-          {listItem.location && listItem.location.address && (
-            <div>
-              <i aria-hidden="true" className="fa fa-map-marker" />
-              &nbsp;
-              {listItem.location.address.streetAddress &&
-                listItem.location.address.streetAddress
-              }
-              &nbsp;
-              {listItem.location.address.addressLocality &&
-                [listItem.location.address.addressLocality, ","]
-              }
-              &nbsp;
-              {listItem.location.address.addressCountry && (
-                <Link href={`/country/${listItem.location.address.addressCountry}`}>
-                  <span>
-
-                    {translate(listItem.location.address.addressCountry)}
-
-                  </span>
-                </Link>
-              )}
-            </div>
-          )}
           <div className="resultListTitle">
+            <Icon type={listItem['@type']} />
+            &nbsp;
             <Link href={`/resource/${listItem['@id']}`}>
               {translate(listItem.name) || listItem['@id']}
               {listItem.alternateName
@@ -52,8 +32,35 @@ const ResultList = ({ translate, emitter, listItems }) => {
                 : ''}
             </Link>
           </div>
+          {listItem.location && listItem.location.address && (
+            <div className="resultListLocation">
+              <i aria-hidden="true" className="fa fa-map-marker" />
+              {listItem.location.address.streetAddress && (
+                <>
+                  &nbsp;
+                  {listItem.location.address.streetAddress}
+                </>
+              )}
+              {listItem.location.address.addressLocality && (
+                <>
+                  &nbsp;
+                  {[listItem.location.address.addressLocality, ","]}
+                </>
+              )}
+              {listItem.location.address.addressCountry && (
+                <>
+                  &nbsp;
+                  <Link href={`/country/${listItem.location.address.addressCountry}`}>
+                    <span>
+                      {translate(listItem.location.address.addressCountry)}
+                    </span>
+                  </Link>
+                </>
+              )}
+            </div>
+          )}
           {listItem.description && (
-            <div>
+            <div className="resultListDescription">
               {removeMd(translate(listItem.description)).slice(0, 300)}
               ...&nbsp;
               <Link href={`/resource/${listItem['@id']}`}>
