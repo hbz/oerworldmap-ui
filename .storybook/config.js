31cc95da3c8c46f999732ad457159f2523dc2b4f
Daniel Doblado <danieldoblado@gmail.com>
2019-05-22 14:36:23 +0200

    Add main styles to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withA11y } from '@storybook/addon-a11y'
 
+import '../src/styles/main.pcss'
+
 function loadStories() {
   const req = require.context('../stories', true, /\.stories\.js$/)
   req.keys().forEach(filename => req(filename))
