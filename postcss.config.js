3d3b35cb1a1afb17a1cc5ccd358e0435fc0746aa
Daniel Doblado <danieldoblado@gmail.com>
2019-06-24 10:52:02 +0200

    Set browsers as a external file, required by new condiguration

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,6 @@ module.exports = {
     'postcss-import': {},
     'postcss-preset-env': {
       importFrom: 'src/styles/variables.css',
-      browsers: 'last 2 versions',
       features: {
         'nesting-rules': true,
         'custom-media-queries': true,
