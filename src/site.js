1a07364f8268698075c40f3361f741b93f57d673
Daniel Doblado <danieldoblado@gmail.com>
2019-06-27 12:22:03 +0200

    Fixes for site

--- src/site.js
+++ src/site.js
@@ -167,96 +167,98 @@ const createKibanaListener = (() => {
   const init = () => {
     const newWindowLink = document.querySelector('[data-inject-newWindowLink]')
 
-    newWindowLink.addEventListener('click', (e) => {
-      e.preventDefault()
+    if (newWindowLink) {
+      newWindowLink.addEventListener('click', (e) => {
+        e.preventDefault()
 
-      const documentBody = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>Document</title>
-        </head>
-        <body>
-          <iframe
-            src="/kibana/app/kibana#/dashboard/3f24aa90-e370-11e8-bc1a-bd36147d8400?embed=true&_g=()"
-            height="750"
-            width="800"
-            style="border:0; width: 100%; margin: 0 auto;"
-            data-scope="filter.about.@type=Policy"
-          >
-          </iframe>
-          <script>
+        const documentBody = `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+          </head>
+          <body>
+            <iframe
+              src="/kibana/app/kibana#/dashboard/3f24aa90-e370-11e8-bc1a-bd36147d8400?embed=true&_g=()"
+              height="750"
+              width="800"
+              style="border:0; width: 100%; margin: 0 auto;"
+              data-scope="filter.about.@type=Policy"
+            >
+            </iframe>
+            <script>
 
-          const getURL = (route) => {
-            let url = route.path
-            let params = []
-            for (const param in route.params) {
-              const value = route.params[param]
-              if (Array.isArray(value)) {
-                value && (params = params.concat(value.map(value => param + '=' + encodeURIComponent(value))))
-              } else {
-                value && params.push(param + '=' + encodeURIComponent(value))
+            const getURL = (route) => {
+              let url = route.path
+              let params = []
+              for (const param in route.params) {
+                const value = route.params[param]
+                if (Array.isArray(value)) {
+                  value && (params = params.concat(value.map(value => param + '=' + encodeURIComponent(value))))
+                } else {
+                  value && params.push(param + '=' + encodeURIComponent(value))
+                }
               }
+              if (params) {
+                url += '?' + params.join('&')
+              }
+              if (route.hash) {
+                url += '#' + route.hash
+              }
+              return url
             }
-            if (params) {
-              url += '?' + params.join('&')
-            }
-            if (route.hash) {
-              url += '#' + route.hash
-            }
-            return url
+
+            window.addEventListener("message", (msg) => {
+
+              if (msg.data.filter && msg.data.key) {
+
+                const iframe = document.querySelector('iframe')
+                const { scope } = iframe && iframe.dataset
+
+                const info = {
+                  filter: msg.data.filter,
+                  key: msg.data.key,
+                  scope
+                }
+                window.opener.postMessage(info, "*")
+              }
+
+            })
+
+            </script>
+          </body>
+          </html>
+        `
+        const options = 'menubar=no,location=no,resizable=yes,scrollbars=yes,status=yes,width=800,height=750'
+        const newWindow = window.open('', 'OER Policies', options)
+        newWindow.document.write(documentBody)
+        newWindow.document.close()
+      })
+
+      window.addEventListener('message', (msg) => {
+        if (msg.data.filter && msg.data.key) {
+          const iframe = document.querySelector('iframe')
+          const scope = msg.data.scope || (iframe && iframe.dataset && iframe.dataset.scope)
+
+          const params = {
+            [`filter.${msg.data.filter}`]: msg.data.key,
           }
 
-          window.addEventListener("message", (msg) => {
+          if (scope) {
+            const [key, value] = scope.split('=')
+            params[key] = value
+          }
 
-            if (msg.data.filter && msg.data.key) {
-
-              const iframe = document.querySelector('iframe')
-              const { scope } = iframe && iframe.dataset
-
-              const info = {
-                filter: msg.data.filter,
-                key: msg.data.key,
-                scope
-              }
-              window.opener.postMessage(info, "*")
-            }
-
+          window.location.href = getURL({
+            path: '/resource/',
+            params,
           })
-
-          </script>
-        </body>
-        </html>
-      `
-      const options = 'menubar=no,location=no,resizable=yes,scrollbars=yes,status=yes,width=800,height=750'
-      const newWindow = window.open('', 'OER Policies', options)
-      newWindow.document.write(documentBody)
-      newWindow.document.close()
-    })
-
-    window.addEventListener('message', (msg) => {
-      if (msg.data.filter && msg.data.key) {
-        const iframe = document.querySelector('iframe')
-        const scope = msg.data.scope || (iframe && iframe.dataset && iframe.dataset.scope)
-
-        const params = {
-          [`filter.${msg.data.filter}`]: msg.data.key,
         }
-
-        if (scope) {
-          const [key, value] = scope.split('=')
-          params[key] = value
-        }
-
-        window.location.href = getURL({
-          path: '/resource/',
-          params,
-        })
-      }
-    })
+      })
+    }
   }
   return { init }
 })()
