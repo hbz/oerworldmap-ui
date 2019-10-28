2e6afc82ba6b4f0fab8a58b82ef186199d2450af
Daniel Doblado <danieldoblado@gmail.com>
2019-10-28 17:28:13 +0100

    Add view to redirect to edit while changing language

--- src/components/Header.js
+++ src/components/Header.js
@@ -88,13 +88,14 @@ class Header extends React.Component {
       supportedLanguages = SUPPORTED_LANGUAGES
     }
 
+    const view = typeof window !== 'undefined' ? window.location.hash.substr(1) : ''
     const languages = supportedLanguages.filter(lang => lang !== locales[0]).map((lang) => {
       const url = urlParser(Link.self)
       url.searchParams.set('language', lang)
 
       return (
         <li key={lang}>
-          <a href={url.href}>{translate(lang)}</a>
+          <a href={`${url.href}${view ? `#${view}` : ''}`}>{translate(lang)}</a>
         </li>
       )
     })
