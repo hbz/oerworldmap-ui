673d5fb2faff7be627c08053c0292f9cbdadc5d3
Felix Ostrowski <felix.ostrowski@googlemail.com>
2020-08-18 15:38:54 +0200

    Fix malformed query string
    which prevented the language switcher from working properly.

--- src/server.js
+++ src/server.js
@@ -92,11 +92,9 @@ server.use((req, res, next) => {
 
 // Middleware to set public URL
 server.use((req, res, next) => {
-  req.location = new URL(url.format({
-    protocol: req.get('x-forwarded-proto') || req.protocol,
-    host: req.get('x-forwarded-host') || req.get('host'),
-    pathname: req.originalUrl,
-  }))
+  const protocol = req.get('x-forwarded-proto') || req.protocol
+  const host = req.get('x-forwarded-host') || req.get('host')
+  req.location = new URL(`${protocol}://${host}${req.originalUrl}`)
   next()
 })
 
