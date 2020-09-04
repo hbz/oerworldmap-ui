7a80a9ba862355e7e76138aefc06a114db3bb511
Jure Cuhalev <jure@cuhalev.com>
2020-09-04 17:00:38 +0200

    Implements additional fields into CSV export

--- src/components/Export.js
+++ src/components/Export.js
@@ -5,13 +5,56 @@ import withI18n from './withI18n'
 
 import '../styles/components/Export.pcss'
 
+const CsvFields = [
+  "/@id",
+  "/@type",
+  "/name/en",
+  "/name/de",
+  "/name/pt",
+  "/name/es",
+  "/description/en",
+  "/description/de",
+  "/description/pt",
+  "/description/es",
+  "/provider/\\d+/name/en",
+  "/url",
+  "/additionalType/\\d+/name/en",
+  "/primarySector/\\d+/name/en",
+  "/startDate",
+  "/endDate",
+  "/startTime",
+  "/endTime",
+  "/agent/\\d+/name/en",
+  "/location/\\d+/address/.*",
+]
+
 const Export = ({ _links, translate }) => (
   <div className="Export">
     <h2>{translate('export.export')}</h2>
     <div className="exportContent">
-      {_links.refs.map(link => (
-        <a key={link.uri} className="btn" href={link.uri}>{translate(link.type)}</a>
-      ))}
+      {
+        _links.refs.map((link) =>
+          link.type === "text/csv" ? (
+            <a key={link.uri} className="btn"
+              onClick={() => {
+                fetch(link.uri, {
+                  headers: {"X-CSV-HEADERS": CsvFields.join(',')}
+                }).then( res => res.blob() )
+                  .then( blob => {
+                    let file = window.URL.createObjectURL(blob);
+                    window.location.assign(file);
+                  })
+              }}
+            >
+              {translate(link.type)}
+            </a>
+          ) : (
+            <a key={link.uri} className="btn" href={link.uri}>
+              {translate(link.type)}
+            </a>
+          )
+        )
+      }
     </div>
   </div>
 )
