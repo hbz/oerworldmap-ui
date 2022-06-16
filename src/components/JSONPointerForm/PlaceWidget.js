368ea9b094a09ba66592fe133437989a0cdb44fe
Adam Wight <adam.wight@wikimedia.de>
2022-06-16 13:40:24 +0200

    Don't require country input

--- src/components/JSONPointerForm/PlaceWidget.js
+++ src/components/JSONPointerForm/PlaceWidget.js
@@ -125,7 +125,6 @@ class PlaceWidget extends React.Component {
                 options={schema.properties.address.properties.addressCountry.enum}
                 title={schema.properties.address.properties.addressCountry.title}
                 setValue={country => setValue({ address: { addressCountry: country } })}
-                required
               />
               {getProp(['address', 'addressCountry'], value) && (
                 <div>
